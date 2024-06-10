a358cb731c3ed342a118152c1c343a79126d8273
tingkatin <muhammad.nabil@superindo.co.id>
2024-06-10 12:45:10 +0700

    Better error handling and logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	// create necessary loggers
+	// create logs dir first
+	err := os.MkdirAll("./logs/", 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.OpenFile("./logs/application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic(err)
+	}
+	infoLog := log.New(file, "INFO \t", log.Ldate|log.Ltime)
+	errorLog := log.New(file, "ERROR \t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	addr := flag.String("addr", "localhost:8080", "HTTP Server Address")
+	flag.Parse()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -14,8 +32,13 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	fmt.Println("Starting server on port:8080...")
-	err := http.ListenAndServe("localhost:8080", mux)
-	log.Fatal(err)
+	server := http.Server{
+		Addr:     *addr,
+		Handler:  mux,
+		ErrorLog: errorLog, // only for HTTP errors
+	}
 
+	infoLog.Printf("Starting server on %s...", *addr)
+	err = server.ListenAndServe()
+	errorLog.Fatal(err)
 }
